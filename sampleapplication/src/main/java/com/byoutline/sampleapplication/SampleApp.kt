1cf3a243399d75e8b42155cd4b62bafe349364ae
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2017-12-01 19:16:04 +0100

    Refactor: remove unused static component from Sample

--- sampleapplication/src/main/java/com/byoutline/sampleapplication/SampleApp.kt
+++ sampleapplication/src/main/java/com/byoutline/sampleapplication/SampleApp.kt
@@ -31,24 +31,10 @@ open class SampleApp : ActivityInjectorApp() {
     @VisibleForTesting
     @Synchronized
     open fun setComponents(mainComponent: AppComponent) {
-        component = mainComponent
-        component.inject(this)
+        mainComponent.inject(this)
         SecretSauceSettings.set(debug = BuildConfig.DEBUG,
                 containerViewId = R.id.container,
                 bindingViewModelId = BR.viewModel,
                 viewModelFactoryProvider = { viewModelFactory })
-    }
-
-    companion object {
-        /**
-         * Although unusually public static variable is discouraged we keep it,
-         * since calls to component look much better as
-         * `SampleApp.component.inject(this)`
-         *
-         * than alternative:
-         * `SampleApp.getInstance(context).getComponent().inject(this)`
-         */
-        lateinit var component: AppComponent
-            private set
     }
 }
